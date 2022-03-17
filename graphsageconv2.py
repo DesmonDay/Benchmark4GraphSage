7b3a9ba9d7d05fee098adf576acffff2ecf8320e
DesmonDay <9098660116@qq.com>
2022-03-17 12:39:59 +0800

    speed up

--- graphsageconv2.py
+++ graphsageconv2.py
@@ -13,17 +13,17 @@ class GraphSageConv2(nn.Layer):
         super(GraphSageConv2, self).__init__()
         self.pool_type = pool_type
         self.normalize = normalize
-        self.neigh_linear = nn.Linear(input_size, hidden_size)
-        self.self_linear = nn.Linear(input_size, hidden_size, bias_attr=False)
+        self.neigh_linear = nn.Linear(input_size, hidden_size, bias_attr=False)
+        self.self_linear = nn.Linear(input_size, hidden_size)
 
     def forward(self, graph, x, act=None):
         if isinstance(x, paddle.Tensor):
             x = (x, x)
 
-        src, dst = graph.edges[:, 0], graph.edges[:, 1]
-        neigh_x = graph_send_recv(x[0], src, dst, pool_type=self.pool_type,
-                                  out_size=paddle.max(dst) + 1)
-        output = self.neigh_linear(neigh_x)
+        src, dst = graph
+        output = graph_send_recv(x[0], src, dst, pool_type=self.pool_type,
+                                  out_size=x[1].shape[0])
+        output = self.neigh_linear(output)
         output += self.self_linear(x[1])
         if self.normalize:
             output = F.normalize(output, axis=1)
